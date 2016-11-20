87c418b7eb43ae45484c7a38a8a6218cadcd725a
SmugDuck149 <raunakshah13@gmail.com>
2016-11-20 16:57:57 +0530

    404 Status detector

--- 404Detector.js
+++ 404Detector.js
@@ -1,65 +1,54 @@
+/* Crawler to detect 404 Page Not Found errors */
 
 var express  = require('express');
-var app      = express();                               // create our app w/ express
+var app      = express();                               // create the app with express
 var port = process.env['PORT'] || 3000;
 var http = require('http');
 var https = require('https');
-/*var postData = querystring.stringify({
-  'msg' : 'Hello World!'
-});
-*/
 var data = ''
-var moved = []
 var crawlLinks = []
 var linkNumber = 0
 crawlLinks[0] = 'sitemap.xml'
-var j= 0;
-var wrongUrl = ''
+var urlNotFoundList = ''
 var crawler = new Object;
-var flag = 0;
+
 function request(url){
-	//console.log("j=" + j + ' ' + url);
-	console.log(linkNumber);
 	console.log(url);
 	if(url.indexOf("https") == 0)
 		return;
 	var req = http.request(url, function(res) {
 	  console.log('STATUS: ' + res.statusCode);
-	  //console.log('HEADERS: ' + JSON.stringify(res.headers));
-	  /*if(res.statusCode==404){
-		  wrongUrl += url;
-		  console.log("wrong urls:" + wrongUrl);
+	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+	  if(res.statusCode==404){												// Status code = 404 indicates Page Not Found
+		  urlNotFoundList += url; 											//Add current url to list of Not Founds.
+		  console.log("incorrect url:" + urlNotFoundList);
 		  request(expressionMatch[linknumber++]);
 	  }
 	  else
-		*/  
-	  res.setEncoding('utf8');
+		res.setEncoding('utf8');
 	  
 	  res.on('data', function (chunk) {
 		data += chunk;
-		console.log('BODY: ' + data);
+		//console.log('BODY: ' + data);
 		
 	  });
 	  
+//Locate website links on current url 	  
 	  res.on('end', function() {
 			if(linkNumber == 0){	
 				var regex = /http(.*)([^s])/
 				var expressionMatch = data.match(regex);
-				console.log(expressionMatch);
-				crawlUrl = expressionMatch[linkNumber++];
-				request(crawlUrl);			
+				//console.log(expressionMatch);
 			}
-			else
-				
-					crawlUrl = expressionMatch[linkNumber++];
-					request(crawlUrl);			
-	});
+			crawlUrl = expressionMatch[linkNumber++];				//Next url to crawl
+			request(crawlUrl);						
+		});
 
 		req.on('error', function(e) {
 			console.log('problem with request: ' + e.message);
 
 		});
-	req.end();
+		req.end(); 							
 	});
 }
 request("http://www.rrap-software.com/sitemap.xml");
