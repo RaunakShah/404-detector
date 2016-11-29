ca606aa8a09ec9b2dabc4087b7a1c49ac7c24a97
SmugDuck149 <raunakshah13@gmail.com>
2016-11-29 15:05:47 +0530

    301 Status Detector

--- 301Detector.js
+++ 301Detector.js
@@ -6,82 +6,70 @@ var port = process.env['PORT'] || 3000;
 var http = require('http');
 var https = require('https');
 var data = ''
-var moved = [] 					// array of urls that have been relocated
-var crawlLinks = []				//List of url's in line 
+var movedUrl = []  //Array of urls that have been relocated											
 var linkNumber = 0
-crawlLinks[0] = 'index.html'
 var counter = 0;
 var crawler = new Object;
 var flag = 0;
 function request(url){
-	counter++
-	var options = {
-	  hostname: 'www.rrap-software.com',
-	  port: 80,
-	  path: crawlLinks[linkNumber],
-	  headers: {
-		'Content-Type': 'application/x-www-form-urlencoded',
-	  }
-	};
-	if(url.indexOf("https") == 0)			//End of crawl list
+	counter++;
+	//End of crawl list
+	if(url.indexOf("https") == 0)						
 		return;
 	var req = http.request(url, function(res) {
-	  console.log('STATUS: ' + res.statusCode);
-	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-	  if(res.statusCode == 301){  						//Status code = 301, moved permanently
-		  console.log(url + ' has been moved to ' + JSON.stringify(res.headers.location)); // res.headers.location contains new location
-		  moved.push(url);
-		  //console.log(moved.length);
-	  }
-	  res.setEncoding('utf8');
-	  res.on('data', function (chunk) {
-		data += chunk;
-		//console.log('BODY: ' + data);		
-	  });
+		console.log('STATUS: ' + res.statusCode);
+		//Status code = 301, moved permanently
+		if(res.statusCode == 301){  							
+			// res.headers.location contains new location
+			console.log(url + ' has been moved to ' + JSON.stringify(res.headers.location)); 
+			movedUrl.push(url);
+		 }
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			data += chunk;
+			//console.log('BODY: ' + data);		
+		});
 	  
-//Locate website links on current url 
-	  res.on('end', function() {
+		//Locate website links on current url 
+		res.on('end', function() {
 			var regEx = /<a[\s]?href[\s]?=[\s]?"([^<]*)">/g
 			var regEx2 = /(.*)"([^\s]*)"/
 			var regEx3 = /"/
-			var expressionMatch = data.match(regEx);
-			var expressionSplit = new Array(expressionMatch.length);
-			var expressionSplit2 = new Array(expressionMatch.length);
+			var expressionMatch = data.match(regEx); 					
+			var expressionSplit = new Array(expressionMatch.length);		 
+			var extractUrl = new Array(expressionMatch.length);			
 			var loopCount = 0; 
 			for (loopCount = 0; loopCount < expressionMatch.length; loopCount++) {
 				expressionSplit[loopCount] = new Array(20);
-				expressionSplit2[loopCount] = new Array(20);
+				extractUrl[loopCount] = new Array(20);
 			}
 			for(loopCount = 0; loopCount < expressionMatch.length; loopCount++){	
-				expressionSplit[loopCount] = expressionMatch[loopCount].split(regex3)
-				expressionSplit2[loopCount] = expressionSplit[loopCount][1].match(/http(.)*/);	
-				//console.log(expressionSplit[loopCount][1]);
+				expressionSplit[loopCount] = expressionMatch[loopCount].split(regEx3)		//Split BODY of response 
+				extractUrl[loopCount] = expressionSplit[loopCount][1].match(/http(.)*/);	//Match Url strings from BODY	
 			}
-			for(loopCount = 0; loopCount < expressionSplit2.length; loopCount++){
-					if(expressionSplit2[loopCount]!=null){
-						var name = expressionSplit2[loopCount][0];
-						console.log(name);
-						crawler[name] = 1	;
+			for(loopCount = 0; loopCount < extractUrl.length; loopCount++){
+					//If new urls exist in BODY
+					if(extractUrl[loopCount]!=null){	
+						var urlString = extractUrl[loopCount][0];
+						crawler[urlString] = 1;
 					}
 			}			
-			for(loopCount=0;loopCount<expressionSplit2.length;loopCount++){
-					if(expressionSplit2[loopCount]!=null){		
-						var name2 = expressionSplit2[loopCount][0];
-						crawler['http://www.facebook.com/ecommerceinindia'] = 2;
-						if(crawler[name2]==1){
-							var crawlUrl = name2
-							crawler[name2]++;
-							//console.log(crawlUrl);
-							if(counter==100){
+			for(loopCount=0;loopCount<extractUrl.length;loopCount++){
+					if(extractUrl[loopCount]!=null){		
+						var urlString = extractUrl[loopCount][0];
+						if(crawler[urlString]==1){
+							var crawlUrl = urlString;
+							crawler[urlString]++;	//Increment to avoid repeats
+							if(counter==100){		//Set max limit for crawler
 								flag = 1;
 								break;
 							}
 							else								
-							request(crawlUrl);
+							request(crawlUrl);		//Next url to crawl 
 						}
-						else
+						else	//Url has already been encountered and crawler function has been performed
 						{
-							console.log("repeat! " + name2);
+							console.log("Repeated URl: " + urlString);
 						}
 					}
 						if(flag==1)
@@ -94,8 +82,13 @@ function request(url){
 		console.log('problem with request: ' + e.statusCode);
 	});
 
-// write data to request body
-//request.write(postData);
 req.end();
 }
-request("http://www.rrap-software.com/index.html");
+
+//Command line argument to contain source url
+if (process.argv.length <= 2) {
+    console.log("Add source url to command line");
+    process.exit(-1);
+}
+//Begin crawl
+request(process.argv[2]);
